19ad99babe495891af3880c8849e0edbfab9d808
Kairatbek23 <82087081+Kairatbek23@users.noreply.github.com>
2021-04-20 17:08:49 +0600

    Add files via upload

---
+++ homework_homework3.py
@@ -0,0 +1,123 @@
+# Вставляйте решение под задачей
+
+# К просмотре обязательно ---- https://www.youtube.com/watch?v=zZBiln_2FhM
+
+# Создайте массив состоящий из 5ти словарей
+# --- > Здесь опишите решение
+mas = [{
+    'banana' : 'yellow',
+    'apple' : 'green or red',
+    'strawberry' : 'pink',
+    'kiwi' : 'brown',
+    'potato' : 'brown'
+
+}]
+
+
+# Создайте массив состоящий из 5ти массивов с любыми значениями
+# --- > Здесь опишите решение
+
+smth = [ ['Osh', 8, 156, 'Bishkek', 'Moscow', 15], ['Kanye', 'Biber', 'Beyonce', 'KendallJenner', 'KimKard' ],[1],[2], [32]]
+
+
+# Замените в ранее созданном массиве несколько значений
+# --- > Здесь опишите решение
+
+smth = [ ['one', 'two'], ['Kanye', 'Biber', 'Beyonce', 'KendallJenner', 'KimKard' ],['BMW', 'Mers', 'Honda']]
+
+
+# Создайте цикл для первого масссива
+# --- > Здесь опишите решение
+
+for i in smth[0]:
+    i = i
+    print(i)
+
+# Создайте цикл для второго масссива
+# --- > Здесь опишите решение
+for i in smth[1]:
+    i = i
+    print(i)
+
+# Создайте массив состоящий из 10 цифр от 0 и до 20
+# --- > Здесь опишите решение
+num = [1,2,3,4,5,6,7,8,9,0,1,0,11,12,13,14,15,16,17,18,19,20]
+
+
+# Пройдитесь циклоом по вышесозданному муссиву и выведите все чита которые больше 10ти
+# --- > Здесь опишите решение
+
+for k in num:
+    k = k
+    if k >= 10:
+        print(k)
+
+# Создайте словарь который содержит в себе все типы данных которые мы проходили (ключи на ваше усмотрение)
+# --- > Здесь опишите решение
+
+nasa = {
+    'banana' : [2],
+    'apple' : 'string',
+    'strawberry' : 'pink',
+    'kiwi' : 'brown',
+    'potato' : 'brown'
+
+}
+
+# Создайте словарь который содержит в себе 5 разных массивов
+# --- > Здесь опишите решение
+nasada = {
+    'banana' : [2],
+    'apple' : [432],
+    'strawberry' : [453],
+    'kiwi' : ['dsfa'],
+    'potato' : ['sdfasdf']
+}
+
+
+
+# Создайте условия if / else
+# --- > Здесь опишите решение
+
+height= 180
+if height > 175:
+    print("Accept")
+else:
+    print("NOt accetpted ")
+# Создайте условия if / elif / else
+# --- > Здесь опишите решение
+
+n = 185
+if n < 190:
+    print("No")
+elif n%2 == 1:
+    print("Yes")
+else:
+    print("No way")
+
+# Создайте условия if / elif / elif / else
+# --- > Здесь опишите решение
+
+a = 10
+b = 20
+c = 55
+if a + b + c > 100:
+    print("Big one")
+elif a + c > 60:
+    print("Bigger than 60")
+elif b +c :
+    print("Bigger than 70")
+else:
+    prinT("HELLO MALIK, HI MALIK--------JUST JOKE")
+
+# Создайте условия цикл который проходится по массиву в который содержит в себе условия if / elif / else
+# --- > Здесь опишите решение
+V =[100, 200, 300]
+for q in V:
+    q = q
+    if q > 100:
+        print("Bigger than 100")
+    elif q >=200:
+        print("Bigger than 200")
+    else:
+        print("None of them")
